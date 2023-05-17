e105bef8e92d97d6db621bb29c04841d5754f557
truongduy28 <truongduy.vithanhcity@gmail.com>
2023-05-17 12:35:10 +0700

    add logo to asset for git

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-native/no-inline-styles */
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, View} from 'react-native';
 import React from 'react';
 import IconEntypo from 'react-native-vector-icons/Entypo';
 import IconMaterialIcons from 'react-native-vector-icons/MaterialIcons';
@@ -86,5 +86,3 @@ const AppHeader = () => {
 };
 
 export default AppHeader;
-
-const styles = StyleSheet.create({});
